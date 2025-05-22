df8ddf52377c0f1459d64bdb22301abf93525419
Mohammad Amin <m.amin.arjang@gmail.com>
2025-05-22 15:11:24 +0330

    add list to items

--- src/app/dashboard/addTicket/page.tsx
+++ src/app/dashboard/addTicket/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Main from "@/components/main";
 import NavBar from "@/components/navBar";
 import SideBar from "@/components/sidebar";
@@ -17,8 +17,14 @@ import {
 } from "@mui/material";
 
 export default function AddTicket() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const handleSubmit = () => {};
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setSelectedFile(e.target.files[0]);
+    }
+  };
   const handlUploadButtonClick = () => {
     fileInputRef.current?.click();
   };
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,67 +1,110 @@
-import { Delete, Edit, Visibility } from "@mui/icons-material";
-import { Box, IconButton, List, ListItem, ListItemButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Delete, Edit, Logout, Settings, Visibility } from "@mui/icons-material";
+import {
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import Link from "next/link";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 
-export default function SideBar(){
-    return(<Box
+export default function SideBar() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        maxWidth: "100%",
+        justifyContent: "end",
+        marginLeft: "5rem",
+        marginTop: "2rem",
+        gap: 5,
+      }}
+    >
+      <Paper
         sx={{
-          display: "flex",
-          maxWidth: "100%",
-          justifyContent: "end",
-          marginLeft: "5rem",
-          marginTop: "2rem",
-          gap: 5,
+          marginTop: "1 rem",
+          backgroundColor: "rgb(245 248 254)",
+          width: "300px",
+          height: "80vh",
+          boxShadow: 0,
+          borderRadius: 5,
         }}
       >
-        
-
-        <Paper
-          sx={{
-            marginTop: "1 rem",
-            backgroundColor: "rgb(245 248 254)",
-            width: "300px",
-            height: "80vh",
-            boxShadow: 0,
-            borderRadius: 5,
-          }}
-        >
-          <List sx={{ color: "rgb(85 118 139)" }}>
-            <ListItem>
-              <Link href={"/dashboard/addTicket"}>
-                <ListItemButton
-                  sx={{
-                    gap: 2,
-                    marginRight: "1rem",
-                    "&:hover": {
-                      backgroundColor: "rgb(66 195 223 / .08)",
-                      borderRadius: 3,
-                    },
-                  }}
-                >
-                  <AddCircleIcon />
-                  تیکت جدید
-                </ListItemButton>
-              </Link>
-            </ListItem>
-            <ListItem>
-              <Link href={"/dashboard"}>
-                <ListItemButton
-                  sx={{
-                    gap: 2,
-                    marginRight: "1rem",
-                    "&:hover": {
-                      backgroundColor: "rgb(66 195 223 / .08)",
-                      borderRadius: 3,
-                    },
-                  }}
-                >
-                  <Visibility />
-                  مشاهده همه تیکت ها
-                </ListItemButton>
-              </Link>
-            </ListItem>
-          </List>
-        </Paper>
-      </Box>);
+        <List sx={{ color: "rgb(85 118 139)" }}>
+          <ListItem>
+            <Link href={"/dashboard/addTicket"}>
+              <ListItemButton
+                sx={{
+                  gap: 2,
+                  marginRight: "1rem",
+                  "&:hover": {
+                    backgroundColor: "rgb(66 195 223 / .08)",
+                    borderRadius: 3,
+                  },
+                }}
+              >
+                <AddCircleIcon />
+                تیکت جدید
+              </ListItemButton>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href={"/dashboard"}>
+              <ListItemButton
+                sx={{
+                  gap: 2,
+                  marginRight: "1rem",
+                  "&:hover": {
+                    backgroundColor: "rgb(66 195 223 / .08)",
+                    borderRadius: 3,
+                  },
+                }}
+              >
+                <Visibility />
+                مشاهده همه تیکت ها
+              </ListItemButton>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href={"/dashboard/settings"}>
+              <ListItemButton sx={{
+                  gap: 2,
+                  marginRight: "1rem",
+                  "&:hover": {
+                    backgroundColor: "rgb(66 195 223 / .08)",
+                    borderRadius: 3,
+                  },
+                }}>
+                <Settings />
+                تنظیمات
+              </ListItemButton>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href={"/logout"}>
+            <ListItemButton sx={{
+                  gap: 2,
+                  marginRight: "1rem",
+                  "&:hover": {
+                    backgroundColor: "rgb(66 195 223 / .08)",
+                    borderRadius: 3,
+                  },
+                }}>
+              <Logout />
+              خروج
+            </ListItemButton>
+            </Link>
+          </ListItem>
+        </List>
+      </Paper>
+    </Box>
+  );
 }
